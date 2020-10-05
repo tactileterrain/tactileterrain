2e53768d037be4d5658353ec15996e0f3bff55c6
joseherminiocollas <joseherminiocollas@goatstone.com>
2020-10-04 17:51:24 -0700

    use set of endpoints

--- src/BingMap/index.js
+++ src/BingMap/index.js
@@ -17,44 +17,61 @@ const BingMap = ({
       center: new window.Microsoft.Maps.Location(...seattle),
       // eslint-disable-next-line
       mapTypeId: window.Microsoft.Maps.MapTypeId.aerial,
-      zoom: 6,
-      // showLocateMeButton: false,
-      // disableStreetside: true,
-      // disableBirdseye: true,
-      // disableKeyboardInput: true,
-      // showZoomButtons: false,
+      zoom: 2,
+      showLocateMeButton: false,
+      disableStreetside: true,
+      disableBirdseye: true,
+      disableKeyboardInput: true,
+      showZoomButtons: false,
     })
     map.setOptions({
-      // showMapTypeSelector: false,
-      // showScalebar: false,
-      // allowHidingLabelsOfRoad: true,
-      // showMapLabels: false,
+      showMapTypeSelector: false,
+      showScalebar: false,
+      allowHidingLabelsOfRoad: true,
+      showMapLabels: false,
       // disableZooming: true,
       // disablePanning: true
     })
     const pr = {
-        'temp': 'AIRS_L2_Temperature_500hPa_Day',
-        'particulate': 'Particulate_Matter_Below_2.5micrometers_2010-2012',
-        'rain': 'A2_RainOcn_NRT',
-        'wind': 'AMSR2_Wind_Speed_Day',
-        'albedo': 'MERRA2_Surface_Albedo_Monthly',
-        'elevation': 'ASTER_GDEM_Color_Index',
-        'soil_moisture': 'Aquarius_Soil_Moisture_Daily',
-        'pressure': 'MERRA2_Surface_Pressure_Monthly',
-        'population': 'GPW_Population_Density_2020'
-      }
-    const products = [
-      'MODIS_Terra_CorrectedReflectance_TrueColor',
-      'VIIRS_Black_Marble',
+      'temp': 'AIRS_L2_Temperature_500hPa_Day',
+      'particulate': 'Particulate_Matter_Below_2.5micrometers_2010-2012',
+      'rain': 'A2_RainOcn_NRT',
+      'wind': 'AMSR2_Wind_Speed_Day',
+      'albedo': 'MERRA2_Surface_Albedo_Monthly',
+      'elevation': 'ASTER_GDEM_Color_Index',
+      'soil_moisture': 'Aquarius_Soil_Moisture_Daily',
+      'pressure': 'MERRA2_Surface_Pressure_Monthly',
+      'population': 'GPW_Population_Density_2020'
+    }
+    // 'MODIS_Terra_CorrectedReflectance_TrueColor',
+    // 'VIIRS_Black_Marble',
+    const atmosphereProducts = [
       'MODIS_Terra_Aerosol',
       'OMI_Absorbing_Aerosol_Optical_Thickness_MW_388',
       'MODIS_Terra_AOD_Deep_Blue_Land'
     ]
     const espgs = ['epsg4326', 'epsg3857']
+    // tilematrix set invalid parameter
+    var atmosphereTileSource = new window.Microsoft.Maps.TileSource({
+      uriConstructor: tile => {
+        return `https://gibs.earthdata.nasa.gov
+/wmts/
+${espgs[1]}
+/best/
+${atmosphereProducts[2]}
+/default/2014-04-09/GoogleMapsCompatible_Level6/
+${tile.zoom}/${tile.y}/${tile.x}
+.png`
+      },
+      minZoom: 1,
+      maxZoom: 16,
+    });
 
     var fireTileSource = new window.Microsoft.Maps.TileSource({
       uriConstructor: tile => {
-        return `https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/MODIS_Terra_Aerosol/default/2014-04-09/GoogleMapsCompatible_Level6/${tile.zoom}/${tile.y}/${tile.x}.png`
+        return `https://gibs.earthdata.nasa.gov/
+wmts/epsg3857/best/MODIS_Terra_Aerosol/default/2014-04-09/GoogleMapsCompatible_Level6/
+${tile.zoom}/${tile.y}/${tile.x}.png`
       },
       minZoom: 1,
       maxZoom: 16,
@@ -76,7 +93,7 @@ const BingMap = ({
       bounds: window.Microsoft.Maps.LocationRect.fromEdges(35.176, -101.065, 14.01, -80.538)
   });
     var layer = new window.Microsoft.Maps.TileLayer({
-      mercator: fireTileSource,
+      mercator: atmosphereTileSource,
       opacity: 1.0
     });
     map.layers.insert(layer);    
